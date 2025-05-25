0981d24ccc2ac3a729db7a32ef2c69ac29a94cb6
subham1120 <subham.sahu@aisthetic.co>
2025-05-25 15:16:38 +0530

    refactor: move next-santity to frontend folder

--- frontend/next-sanity/sanity.cli.ts
+++ frontend/next-sanity/sanity.cli.ts
@@ -1,7 +1,6 @@
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+import { dataset, projectId } from '@/sanity/env'
 
 export default defineCliConfig({
     api: { projectId, dataset },
@@ -9,11 +8,6 @@ export default defineCliConfig({
         define: {
             "process.env.NEXT_PUBLIC_SANITY_PROJECT_ID": JSON.stringify(projectId),
             "process.env.NEXT_PUBLIC_SANITY_DATASET": JSON.stringify(dataset),
-        },
-        resolve: {
-            alias: {
-                "@": __dirname,
-            },
-        },
+        }
     },
 });
